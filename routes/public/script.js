1159858ab94d8095ef3f10c82e3846d86e833a75
Nimish Santosh <nimishsantosh107@icloud.com>
2019-08-26 12:12:51 +0530

    peer data sending done

--- routes/public/script.js
+++ routes/public/script.js
@@ -16,9 +16,34 @@ function deletePeer(peerid) {
 		peersList.splice(index,1);
 }
 
+function deleteConnObj(peerid) {
+	var index = -1;
+	for(var i = 0 ; i < connObjList.length ; i++){
+		if(connObjList[i].peer === peerid)
+			index = i;
+	}
+	if (index !== -1)
+		connObjList.splice(index,1);
+}
+
+function sendMessage() {
+	var msg = messageSendText.value;
+	updateMessageList(msg);
+	connObjList.forEach(function (connObj) {
+		connObj.send(msg);
+	});
+}
+
+function updateMessageList(msg) {
+	var p = document.createElement("p");
+	p.innerText = msg;
+	messages.append(p);
+}
+
 var socket = io(window.location.origin);
 var peer = null;
 var peersList = [];
+var connObjList = [];
 
 socket.on('connect', function () {
 	console.log('CONNECTED');
@@ -30,8 +55,11 @@ socket.on('connect', function () {
 		else{
 			//PEER LEAVING CURRENT ROOM / JOINING NEW
 			peersList = [];
-			if(socket.room)
+			connObjList = [];
+			if(socket.room){
+				messages.innerHTML = "";
 				socket.emit('leaving', {peerid: peer.id});
+			}
 			socket.room = roomName.value;
 			socket.emit('joinRoom',{roomName: socket.room});
 
@@ -42,7 +70,18 @@ socket.on('connect', function () {
 			peer.on('open', function (id) {
 				peer.id = id;
 				socket.emit('signal',{peerid: peer.id});
+				messageContainer.style.display = "block";
 				console.log(`PEER ID: ${peer.id}`);
+			});
+
+			//RECIEVE CONNECTION ***********
+			peer.on('connection', function (conn) {
+				//SET DATA LISTENER 
+			  	conn.on('data', function(data) {
+			  		updateMessageList("PEER: "+data);
+			  	});
+				//PUSH CONN TO CONN LIST FINALLY
+				connObjList.push(conn);							
 			});
 		}
 	});
@@ -54,9 +93,21 @@ socket.on('connect', function () {
 		console.log(data);
 	});
 
-	//ACCEPT NEW PEER CONNECTION
-	socket.on('newSignal', function (data) {
+	//GET NEW PEER CONNECTION AND MAKE CONNECTION TO NEW CLIENT
+	socket.on('newSignal', async function (data) {
 		peersList.push(data);
+		//CONNECT TO PEER
+		var conn = await peer.connect(data.peerid);
+		//CONN METHODS  ***********
+		conn.on('open', function () {
+			//SET DATA LISTENER
+		  	conn.on('data', function(data){
+		  		updateMessageList("PEER: "+data);
+		  	});	
+			//PUSH TO PEER OBJ LIST
+			connObjList.push(conn);
+		});
+
 		socket.emit('returnSignal', {destSocket: data.socketid, peerid: peer.id});
 	});
 
@@ -67,7 +118,8 @@ socket.on('connect', function () {
 
 	//DELETE LEFT USER FROM PEERSLIST
 	socket.on('newLeaving', function (data) {
-		deletePeer(data.leftPeerid); //****************************************
+		deletePeer(data.leftPeerid);
+		deleteConnObj(data.leftPeerid);
 		console.log(`${data.leftPeerid} LEFT THE ROOM`);
 	});
 });
