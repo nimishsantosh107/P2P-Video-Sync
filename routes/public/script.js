356fd849a8067fb077c19d66b72ddde0e95d4a68
Nimish Santosh <nimishsantosh107@icloud.com>
2019-08-27 23:25:40 +0530

    unreliable?

--- routes/public/script.js
+++ routes/public/script.js
@@ -208,7 +208,7 @@ function getAudioStream () {
 	}, function (err) {console.log(`ERROR GETTING STREAM`);});
 }
 
-function getCallStream (stream) {
+function getCallStream (call, stream) {
 	console.log("RECIEVED: ",stream);
 	var audioTag = document.createElement("audio");
 	audioTag.id = call.peer;
@@ -283,7 +283,7 @@ socket.on('connect', async function () {
 			peer.on('call', function (call) {
 				//GET REMOTE STREAM
 				call.on('stream',function(stream){
-					getCallStream(stream);
+					getCallStream(call, stream);
 							
 					//PUSH TO CALL OBJ ARRAY
 					deleteCallObj(call.peer);
@@ -338,7 +338,7 @@ socket.on('connect', async function () {
 		var call = await peer.call(data.peerid, window.stream);
 		//CALL METHODS
 		call.on('stream',function(stream){
-			getCallStream(stream);
+			getCallStream(call, stream);
 
 			//PUSH TO CALL OBJ ARRAY
 			deleteCallObj(call.peer);
